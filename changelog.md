8ac500376b51d8c43fc830f30781abfb2c258d57
Thiago Navarro <thiago@oxyoy.com>
2023-06-15 14:03:55 +0000

    Added CLI example

--- changelog.md
+++ changelog.md
@@ -3,3 +3,4 @@
 ## Version 0.1.0 (2023/06/15)
 
 - First version
+- Added CLI example
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Parser parser = new Parser('(', ')');
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Inform the enclosing (Default: '()'):");
+	    String enclosing = sc.nextLine();
+	    if (enclosing.length() == 0) enclosing = "()";
+		Parser parser = new Parser(enclosing.charAt(0), enclosing.charAt(1));
+
+	    System.out.println("Type you string:");
+		String[] result = parser.get(sc.nextLine());
+		System.out.println("Found " + result.length + " enclosing texts:");
 		
-		String[] result = parser.get("Hello (simple) World!");
-		System.out.println(result.length);
-		
-		for (String item : result) {
-			System.out.println("\t" + item);			
-		}
+		for (String item : result)
+			System.out.println("\t" + item);
 	}
 }
