54df18ef051173315ee468036be10b3dfbaf719c
Thiago Navarro <thiago@oxyoy.com>
2023-06-15 14:08:21 +0000

    Formatted the code

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Inform the enclosing (Default: '()'):");
-	    String enclosing = sc.nextLine();
-	    if (enclosing.length() == 0) enclosing = "()";
-		Parser parser = new Parser(enclosing.charAt(0), enclosing.charAt(1));
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Inform the enclosing (Default: '()'):");
+    String enclosing = sc.nextLine();
+    if (enclosing.length() == 0)
+      enclosing = "()";
+    Parser parser = new Parser(enclosing.charAt(0), enclosing.charAt(1));
 
-	    System.out.println("Type you string:");
-		String[] result = parser.get(sc.nextLine());
-		System.out.println("Found " + result.length + " enclosing texts:");
-		
-		for (String item : result)
-			System.out.println("\t" + item);
-	}
+    System.out.println("Type you string:");
+    String[] result = parser.get(sc.nextLine());
+    System.out.println("Found " + result.length + " enclosing texts:");
+
+    for (String item : result)
+      System.out.println("\t" + item);
+  }
 }
